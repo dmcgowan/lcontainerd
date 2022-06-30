ac27eb59a83d6a23f087575da9623d84f57e7be5
Derek McGowan <derek@mcg.dev>
2022-06-29 17:08:57 -0700

    Add db package for local implementations
    Signed-off-by: Derek McGowan <derek@mcg.dev>

---
+++ pkg/db/db.go
@@ -0,0 +1,344 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package db
+
+import (
+	"encoding/binary"
+	"fmt"
+	"sync"
+
+	"github.com/containerd/containerd/content"
+	"github.com/containerd/containerd/errdefs"
+	bolt "go.etcd.io/bbolt"
+)
+
+const (
+	// schemaVersion represents the schema version of
+	// the database. This schema version represents the
+	// structure of the data in the database. The schema
+	// can envolve at any time but any backwards
+	// incompatible changes or structural changes require
+	// bumping the schema version.
+	schemaVersion = "v1"
+
+	// dbVersion represents updates to the schema
+	// version which are additions and compatible with
+	// prior version of the same schema.
+	dbVersion = 1
+)
+
+// DBOpt configures how we set up the DB
+type DBOpt func(*dbOptions)
+
+// dbOptions configure db options.
+type dbOptions struct {
+}
+
+// DB represents a metadata database backed by a bolt
+// database. The database is fully namespaced and stores
+// image, container, namespace, snapshot, and content data
+// while proxying data shared across namespaces to backend
+// datastores for content and snapshots.
+type DB struct {
+	db *bolt.DB
+	cs *contentStore
+
+	// wlock is used to protect access to the data structures during garbage
+	// collection. While the wlock is held no writable transactions can be
+	// opened, preventing changes from occurring between the mark and
+	// sweep phases without preventing read transactions.
+	wlock sync.RWMutex
+
+	// dirty flag indicates that references have been removed which require
+	// a garbage collection to ensure the database is clean. This tracks
+	// the number of dirty operations. This should be updated and read
+	// atomically if outside of wlock.Lock.
+	dirty uint32
+
+	// dirtyCS flags keeps track of datastores which have had
+	// deletions since the last garbage collection. These datastores will
+	// be garbage collected during the next garbage collection. These
+	// should only be updated inside of a write transaction or wlock.Lock.
+	dirtyCS bool
+
+	dbopts dbOptions
+}
+
+// NewDB creates a new metadata database using the provided
+// bolt database, content store, and snapshotters.
+func NewDB(db *bolt.DB, cs content.Store, opts ...DBOpt) *DB {
+	m := &DB{
+		db:     db,
+		dbopts: dbOptions{},
+	}
+
+	for _, opt := range opts {
+		opt(&m.dbopts)
+	}
+
+	// Initialize data stores
+	m.cs = newContentStore(m, cs)
+
+	return m
+}
+
+// ContentStore returns a namespaced content store
+// proxied to a content store.
+func (m *DB) ContentStore() content.Store {
+	if m.cs == nil {
+		return nil
+	}
+	return m.cs
+}
+
+// View runs a readonly transaction on the metadata store.
+func (m *DB) View(fn func(*bolt.Tx) error) error {
+	return m.db.View(fn)
+}
+
+// Update runs a writable transaction on the metadata store.
+func (m *DB) Update(fn func(*bolt.Tx) error) error {
+	m.wlock.RLock()
+	defer m.wlock.RUnlock()
+	err := m.db.Update(func(tx *bolt.Tx) error {
+		if err := updateDBVersion(tx); err != nil {
+			return err
+		}
+
+		return fn(tx)
+		// TODO: Check for cleanup?
+	})
+	return err
+}
+func updateDBVersion(tx *bolt.Tx) error {
+	var (
+		bkt = tx.Bucket([]byte(schemaVersion))
+		err error
+		vb  []byte
+	)
+	if bkt != nil {
+		vb = bkt.Get(bucketKeyDBVersion)
+	} else {
+		bkt, err = tx.CreateBucket([]byte(schemaVersion))
+		if err != nil {
+			return err
+		}
+	}
+	if vb == nil {
+		versionEncoded, err := encodeInt(dbVersion)
+		if err != nil {
+			return err
+		}
+
+		return bkt.Put(bucketKeyDBVersion, versionEncoded)
+	} else {
+		v, _ := binary.Varint(vb)
+		if v != dbVersion {
+			// Here is where migration can happen when bumping the version,
+			// currently only one version exists so not necesesary
+			return fmt.Errorf("wrong version: %d: %w", v, errdefs.ErrFailedPrecondition)
+		}
+
+		return nil
+	}
+}
+
+/*
+// GCStats holds the duration for the different phases of the garbage collector
+type GCStats struct {
+	MetaD     time.Duration
+	ContentD  time.Duration
+	SnapshotD map[string]time.Duration
+}
+
+// Elapsed returns the duration which elapsed during a collection
+func (s GCStats) Elapsed() time.Duration {
+	return s.MetaD
+}
+
+// GarbageCollect removes resources (snapshots, contents, ...) that are no longer used.
+func (m *DB) GarbageCollect(ctx context.Context) (gc.Stats, error) {
+	m.wlock.Lock()
+	t1 := time.Now()
+	c := startGCContext(ctx, m.collectors)
+
+	marked, err := m.getMarked(ctx, c) // Pass in gc context
+	if err != nil {
+		m.wlock.Unlock()
+		return nil, err
+	}
+
+	if err := m.db.Update(func(tx *bolt.Tx) error {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		rm := func(ctx context.Context, n gc.Node) error {
+			if _, ok := marked[n]; ok {
+				return nil
+			}
+
+			if n.Type == ResourceSnapshot {
+				if idx := strings.IndexRune(n.Key, '/'); idx > 0 {
+					m.dirtySS[n.Key[:idx]] = struct{}{}
+				}
+			} else if n.Type == ResourceContent || n.Type == ResourceIngest {
+				m.dirtyCS = true
+			}
+			return c.remove(ctx, tx, n) // From gc context
+		}
+
+		if err := c.scanAll(ctx, tx, rm); err != nil { // From gc context
+			return fmt.Errorf("failed to scan and remove: %w", err)
+		}
+
+		return nil
+	}); err != nil {
+		m.wlock.Unlock()
+		c.cancel(ctx)
+		return nil, err
+	}
+
+	var stats GCStats
+	var wg sync.WaitGroup
+
+	// reset dirty, no need for atomic inside of wlock.Lock
+	m.dirty = 0
+
+	if len(m.dirtySS) > 0 {
+		var sl sync.Mutex
+		stats.SnapshotD = map[string]time.Duration{}
+		wg.Add(len(m.dirtySS))
+		for snapshotterName := range m.dirtySS {
+			log.G(ctx).WithField("snapshotter", snapshotterName).Debug("schedule snapshotter cleanup")
+			go func(snapshotterName string) {
+				st1 := time.Now()
+				m.cleanupSnapshotter(snapshotterName)
+
+				sl.Lock()
+				stats.SnapshotD[snapshotterName] = time.Since(st1)
+				sl.Unlock()
+
+				wg.Done()
+			}(snapshotterName)
+		}
+		m.dirtySS = map[string]struct{}{}
+	}
+
+	if m.dirtyCS {
+		wg.Add(1)
+		log.G(ctx).Debug("schedule content cleanup")
+		go func() {
+			ct1 := time.Now()
+			m.cleanupContent()
+			stats.ContentD = time.Since(ct1)
+			wg.Done()
+		}()
+		m.dirtyCS = false
+	}
+
+	stats.MetaD = time.Since(t1)
+	m.wlock.Unlock()
+
+	c.finish(ctx)
+
+	wg.Wait()
+
+	return stats, err
+}
+
+// getMarked returns all resources that are used.
+func (m *DB) getMarked(ctx context.Context, c *gcContext) (map[gc.Node]struct{}, error) {
+	var marked map[gc.Node]struct{}
+	if err := m.db.View(func(tx *bolt.Tx) error {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		var (
+			nodes []gc.Node
+			wg    sync.WaitGroup
+			roots = make(chan gc.Node)
+		)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for n := range roots {
+				nodes = append(nodes, n)
+			}
+		}()
+		// Call roots
+		if err := c.scanRoots(ctx, tx, roots); err != nil { // From gc context
+			cancel()
+			return err
+		}
+		close(roots)
+		wg.Wait()
+
+		refs := func(n gc.Node) ([]gc.Node, error) {
+			var sn []gc.Node
+			if err := c.references(ctx, tx, n, func(nn gc.Node) { // From gc context
+				sn = append(sn, nn)
+			}); err != nil {
+				return nil, err
+			}
+			return sn, nil
+		}
+
+		reachable, err := gc.Tricolor(nodes, refs)
+		if err != nil {
+			return err
+		}
+		marked = reachable
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return marked, nil
+}
+
+func (m *DB) cleanupSnapshotter(name string) (time.Duration, error) {
+	ctx := context.Background()
+	sn, ok := m.ss[name]
+	if !ok {
+		return 0, nil
+	}
+
+	d, err := sn.garbageCollect(ctx)
+	logger := log.G(ctx).WithField("snapshotter", name)
+	if err != nil {
+		logger.WithError(err).Warn("snapshot garbage collection failed")
+	} else {
+		logger.WithField("d", d).Debugf("snapshot garbage collected")
+	}
+	return d, err
+}
+
+func (m *DB) cleanupContent() (time.Duration, error) {
+	ctx := context.Background()
+	if m.cs == nil {
+		return 0, nil
+	}
+
+	d, err := m.cs.garbageCollect(ctx)
+	if err != nil {
+		log.G(ctx).WithError(err).Warn("content garbage collection failed")
+	} else {
+		log.G(ctx).WithField("d", d).Debugf("content garbage collected")
+	}
+
+	return d, err
+}
+*/
---
+++ pkg/db/db_test.go
@@ -0,0 +1,481 @@
+/*
+   Copyright The containerd Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package db
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math/rand"
+	"path/filepath"
+	"testing"
+
+	"github.com/containerd/containerd/content"
+	"github.com/containerd/containerd/content/local"
+	"github.com/containerd/containerd/errdefs"
+	"github.com/containerd/containerd/leases"
+	"github.com/containerd/containerd/log/logtest"
+	"github.com/containerd/containerd/namespaces"
+	digest "github.com/opencontainers/go-digest"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	bolt "go.etcd.io/bbolt"
+)
+
+type testOptions struct {
+}
+
+type testOpt func(*testOptions)
+
+func testDB(t *testing.T, opt ...testOpt) (context.Context, *DB) {
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = namespaces.WithNamespace(ctx, "testing")
+	ctx = logtest.WithT(ctx, t)
+
+	var topts testOptions
+
+	for _, o := range opt {
+		o(&topts)
+	}
+
+	dirname := t.TempDir()
+
+	cs, err := local.NewStore(filepath.Join(dirname, "content"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bdb, err := bolt.Open(filepath.Join(dirname, "metadata.db"), 0644, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	db := NewDB(bdb, cs)
+	//if err := db.Init(ctx); err != nil {
+	//	t.Fatal(err)
+	//}
+
+	t.Cleanup(func() {
+		bdb.Close()
+		cancel()
+	})
+	return ctx, db
+}
+
+func TestInit(t *testing.T) {
+	_, db := testEnv(t)
+
+	if err := NewDB(db, nil).Update(func(*bolt.Tx) error { return nil }); err != nil {
+		t.Fatal(err)
+	}
+
+	version, err := readDBVersion(db, bucketKeyVersion)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if version != dbVersion {
+		t.Fatalf("Unexpected version %d, expected %d", version, dbVersion)
+	}
+}
+
+/*
+func TestMigrations(t *testing.T) {
+	testRefs := []struct {
+		ref  string
+		bref string
+	}{
+		{
+			ref:  "k1",
+			bref: "bk1",
+		},
+		{
+			ref:  strings.Repeat("longerkey", 30), // 270 characters
+			bref: "short",
+		},
+		{
+			ref:  "short",
+			bref: strings.Repeat("longerkey", 30), // 270 characters
+		},
+		{
+			ref:  "emptykey",
+			bref: "",
+		},
+	}
+	migrationTests := []struct {
+		name  string
+		init  func(*bolt.Tx) error
+		check func(*bolt.Tx) error
+	}{
+		{
+			name: "IngestUpdate",
+			init: func(tx *bolt.Tx) error {
+				bkt, err := createBucketIfNotExists(tx, bucketKeyVersion, []byte("testing"), bucketKeyObjectContent, deprecatedBucketKeyObjectIngest)
+				if err != nil {
+					return err
+				}
+
+				for _, s := range testRefs {
+					if err := bkt.Put([]byte(s.ref), []byte(s.bref)); err != nil {
+						return err
+					}
+				}
+
+				return nil
+			},
+			check: func(tx *bolt.Tx) error {
+				bkt := getIngestsBucket(tx, "testing")
+				if bkt == nil {
+					return fmt.Errorf("ingests bucket not found: %w", errdefs.ErrNotFound)
+				}
+
+				for _, s := range testRefs {
+					sbkt := bkt.Bucket([]byte(s.ref))
+					if sbkt == nil {
+						return fmt.Errorf("ref does not exist: %w", errdefs.ErrNotFound)
+					}
+
+					bref := string(sbkt.Get(bucketKeyRef))
+					if bref != s.bref {
+						return fmt.Errorf("unexpected reference key %q, expected %q", bref, s.bref)
+					}
+				}
+
+				dbkt := getBucket(tx, bucketKeyVersion, []byte("testing"), bucketKeyObjectContent, deprecatedBucketKeyObjectIngest)
+				if dbkt != nil {
+					return errors.New("deprecated ingest bucket still exists")
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name: "NoOp",
+			init: func(tx *bolt.Tx) error {
+				return nil
+			},
+			check: func(tx *bolt.Tx) error {
+				return nil
+			},
+		},
+	}
+
+	if len(migrationTests) != len(migrations) {
+		t.Fatal("Each migration must have a test case")
+	}
+
+	for i, mt := range migrationTests {
+		t.Run(mt.name, runMigrationTest(i, mt.init, mt.check))
+	}
+}
+
+func runMigrationTest(i int, init, check func(*bolt.Tx) error) func(t *testing.T) {
+	return func(t *testing.T) {
+		_, db, cancel := testEnv(t)
+		defer cancel()
+
+		if err := db.Update(init); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := db.Update(migrations[i].migrate); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := db.View(check); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+*/
+
+func readDBVersion(db *bolt.DB, schema []byte) (int, error) {
+	var version int
+	if err := db.View(func(tx *bolt.Tx) error {
+		bkt := tx.Bucket(schema)
+		if bkt == nil {
+			return fmt.Errorf("no version bucket: %w", errdefs.ErrNotFound)
+		}
+		vb := bkt.Get(bucketKeyDBVersion)
+		if vb == nil {
+			return fmt.Errorf("no version value: %w", errdefs.ErrNotFound)
+		}
+		v, _ := binary.Varint(vb)
+		version = int(v)
+		return nil
+	}); err != nil {
+		return 0, err
+	}
+	return version, nil
+}
+
+func bytesFor(i int64) []byte {
+	r := rand.New(rand.NewSource(i))
+	var b [256]byte
+	_, err := r.Read(b[:])
+	if err != nil {
+		panic(err)
+	}
+	return b[:]
+}
+
+func digestFor(i int64) digest.Digest {
+	r := rand.New(rand.NewSource(i))
+	dgstr := digest.SHA256.Digester()
+	_, err := io.Copy(dgstr.Hash(), io.LimitReader(r, 256))
+	if err != nil {
+		panic(err)
+	}
+	return dgstr.Digest()
+}
+
+type object struct {
+	data    interface{}
+	removed bool
+	labels  map[string]string
+}
+
+/*
+func create(obj object, tx *bolt.Tx, db *DB, cs content.Store, sn snapshots.Snapshotter) (*gc.Node, error) {
+	var (
+		node      *gc.Node
+		namespace = "test"
+		ctx       = WithTransactionContext(namespaces.WithNamespace(context.Background(), namespace), tx)
+	)
+
+	switch v := obj.data.(type) {
+	case testContent:
+		expected := digest.FromBytes(v.data)
+		w, err := cs.Writer(ctx,
+			content.WithRef("test-ref"),
+			content.WithDescriptor(ocispec.Descriptor{Size: int64(len(v.data)), Digest: expected}))
+		if err != nil {
+			return nil, fmt.Errorf("failed to create writer: %w", err)
+		}
+		if _, err := w.Write(v.data); err != nil {
+			return nil, fmt.Errorf("write blob failed: %w", err)
+		}
+		if err := w.Commit(ctx, int64(len(v.data)), expected, content.WithLabels(obj.labels)); err != nil {
+			return nil, fmt.Errorf("failed to commit blob: %w", err)
+		}
+		if !obj.removed {
+			node = &gc.Node{
+				Type:      ResourceContent,
+				Namespace: namespace,
+				Key:       expected.String(),
+			}
+		}
+	case testSnapshot:
+		if v.active {
+			_, err := sn.Prepare(ctx, v.key, v.parent, snapshots.WithLabels(obj.labels))
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			akey := fmt.Sprintf("%s-active", v.key)
+			_, err := sn.Prepare(ctx, akey, v.parent)
+			if err != nil {
+				return nil, err
+			}
+			if err := sn.Commit(ctx, v.key, akey, snapshots.WithLabels(obj.labels)); err != nil {
+				return nil, err
+			}
+		}
+		if !obj.removed {
+			node = &gc.Node{
+				Type:      ResourceSnapshot,
+				Namespace: namespace,
+				Key:       fmt.Sprintf("native/%s", v.key),
+			}
+		}
+	case testImage:
+		image := images.Image{
+			Name:   v.name,
+			Target: v.target,
+			Labels: obj.labels,
+		}
+
+		_, err := NewImageStore(db).Create(ctx, image)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create image: %w", err)
+		}
+	case testContainer:
+		container := containers.Container{
+			ID:          v.id,
+			SnapshotKey: v.snapshot,
+			Snapshotter: "native",
+			Labels:      obj.labels,
+
+			Runtime: containers.RuntimeInfo{
+				Name: "testruntime",
+			},
+			Spec: &types.Any{},
+		}
+		_, err := NewContainerStore(db).Create(ctx, container)
+		if err != nil {
+			return nil, err
+		}
+	case testLease:
+		lm := NewLeaseManager(db)
+
+		l, err := lm.Create(ctx, leases.WithID(v.id), leases.WithLabels(obj.labels))
+		if err != nil {
+			return nil, err
+		}
+
+		for _, ref := range v.refs {
+			if err := lm.AddResource(ctx, l, ref); err != nil {
+				return nil, err
+			}
+		}
+
+		if !obj.removed {
+			node = &gc.Node{
+				Type:      ResourceLease,
+				Namespace: namespace,
+				Key:       v.id,
+			}
+		}
+	}
+
+	return node, nil
+}
+
+func blob(b []byte, r bool, l ...string) object {
+	return object{
+		data: testContent{
+			data: b,
+		},
+		removed: r,
+		labels:  labelmap(l...),
+	}
+}
+
+func image(n string, d digest.Digest, l ...string) object {
+	return object{
+		data: testImage{
+			name: n,
+			target: ocispec.Descriptor{
+				MediaType: "irrelevant",
+				Digest:    d,
+				Size:      256,
+			},
+		},
+		removed: false,
+		labels:  labelmap(l...),
+	}
+}
+
+func newSnapshot(key, parent string, active, r bool, l ...string) object {
+	return object{
+		data: testSnapshot{
+			key:    key,
+			parent: parent,
+			active: active,
+		},
+		removed: r,
+		labels:  labelmap(l...),
+	}
+}
+
+func container(id, s string, l ...string) object {
+	return object{
+		data: testContainer{
+			id:       id,
+			snapshot: s,
+		},
+		removed: false,
+		labels:  labelmap(l...),
+	}
+}
+
+func lease(id string, refs []leases.Resource, r bool, l ...string) object {
+	return object{
+		data: testLease{
+			id:   id,
+			refs: refs,
+		},
+		removed: r,
+		labels:  labelmap(l...),
+	}
+}
+*/
+
+type testContent struct {
+	data []byte
+}
+
+type testSnapshot struct {
+	key    string
+	parent string
+	active bool
+}
+
+type testImage struct {
+	name   string
+	target ocispec.Descriptor
+}
+
+type testContainer struct {
+	id       string
+	snapshot string
+}
+
+type testLease struct {
+	id   string
+	refs []leases.Resource
+}
+
+func newStores(t testing.TB) (*DB, content.Store) {
+	td := t.TempDir()
+	db, err := bolt.Open(filepath.Join(td, "meta.db"), 0644, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lcs, err := local.NewStore(filepath.Join(td, "content"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mdb := NewDB(db, lcs)
+
+	t.Cleanup(func() {
+		db.Close()
+	})
+
+	return mdb, mdb.ContentStore()
+}
+
+func testEnv(t *testing.T) (context.Context, *bolt.DB) {
+	ctx, cancel := context.WithCancel(context.Background())
+	ctx = logtest.WithT(ctx, t)
+
+	dirname := t.TempDir()
+
+	db, err := bolt.Open(filepath.Join(dirname, "meta.db"), 0644, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		db.Close()
+		cancel()
+	})
+
+	return ctx, db
+}
